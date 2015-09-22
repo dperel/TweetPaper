cdb770fcff7f19e3aa5ed10adb600169fb261491
dperel <davidperel.pdf@gmail.com>
2015-09-22 13:57:49 -0400

    Twitter client set up. Keys kept safe with Figaro and .gitignore

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,11 @@
 class Article < ActiveRecord::Base
+
+  def create_article
+    #method here will query Twitter API, pull top 5 tweets on the searched subject, and store as attributes of that article
+  end
+
+  def refresh_all
+    
+  end
+
 end

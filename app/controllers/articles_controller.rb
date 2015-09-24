80b271ed1a766fc653eb56b0430c2e51f8a9922e
dperel <davidperel.pdf@gmail.com>
2015-09-23 20:57:00 -0400

    Added article search box in each child div

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -33,7 +33,7 @@ class ArticlesController < ApplicationController
   private
 
   def article_params
-    params.require(:article).permit(:title)
+    params.permit(:title)
   end
 
 
--- app/models/article.rb
+++ app/models/article.rb
@@ -10,19 +10,16 @@ belongs_to :user
         results.push(tweet)
       end
 
-      #need to clean up this mess/mass assignment
-
     @article.title = article_params["title"].capitalize
-    @article.picture_url = results[0].media[0].to_hash[:media_url] if results[0].media?
-    @article.tweet_1 = "#{results[0].user.screen_name}: #{results[0].text}"
-    @article.tweet_2 = "#{results[1].user.screen_name}: #{results[1].text}"
-    @article.tweet_3 = "#{results[2].user.screen_name}: #{results[2].text}"
-    @article.tweet_4 = "#{results[3].user.screen_name}: #{results[3].text}"
-    @article.tweet_5 = "#{results[4].user.screen_name}: #{results[4].text}"
+    @article.picture_url = results[0].media[0].to_hash[:media_url] if (results[0] && results[0].media?)
+    results[0] ? @article.tweet_1 = "#{results[0].user.screen_name}: #{results[0].text}" : @article.tweet_1 = "No tweet :/"
+    results[1] ? @article.tweet_2 = "#{results[1].user.screen_name}: #{results[1].text}" : @article.tweet_1 = "No tweet :/"
+    results[2] ? @article.tweet_3 = "#{results[2].user.screen_name}: #{results[2].text}" : @article.tweet_1 = "No tweet :/"
+    results[3] ? @article.tweet_4 = "#{results[3].user.screen_name}: #{results[3].text}" : @article.tweet_1 = "No tweet :/"
+    results[4] ? @article.tweet_5 = "#{results[4].user.screen_name}: #{results[4].text}" : @article.tweet_1 = "No tweet :/"
     @article.user = current_user
     @article.save
   end
-
 
   def self.establish_client(current_user)
     @@client = Twitter::REST::Client.new do |config|
